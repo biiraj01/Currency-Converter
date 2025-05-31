2165d0553c29ee2c08e658552ff6cac5aa489f4b
biiraj01 <biraj2003pujari@gmail.com>
2025-05-31 18:41:48 +0545

    mes

---
+++ app.js
@@ -0,0 +1,22 @@
+
+const dropDowns = document.querySelectorAll(".dropdown select");
+
+for (let select of dropDowns){
+    for(currCode in countryList){
+        let newOption =document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        select.append(newOption);
+    }
+    select.addEventListener("change",(evt)=>{
+        updateFlag(evt.target);
+    });
+}
+
+const updateFlag = (element)=>{
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+}
